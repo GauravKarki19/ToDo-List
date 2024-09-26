fdacf6a012953ccfecd807c02726bf2d0721b99e
GauravKarki19 <128733402+GauravKarki19@users.noreply.github.com>
2024-09-26 12:16:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,98 +1,42 @@
-const input = document.querySelector("input");
-const addButton = document.querySelector(".add-button");
-const todosHtml = document.querySelector(".todos");
-const emptyImage = document.querySelector(".empty-image");
-let todosJson = JSON.parse(localStorage.getItem("todos")) || [];
-const deleteAllButton = document.querySelector(".delete-all");
-const filters = document.querySelectorAll(".filter");
-let filter = '';
+let listContainer = document.getElementById('list-container')
+let head = document.querySelector('head')
+let inputBox = document.getElementById('input-box')
 
-showTodos();
-
-function getTodoHtml(todo, index) {
-  if (filter && filter != todo.status) {
-    return '';
-  }
-  let checked = todo.status == "completed" ? "checked" : "";
-  return /* html */ `
-    <li class="todo">
-      <label for="${index}">
-        <input id="${index}" onclick="updateStatus(this)" type="checkbox" ${checked}>
-        <span class="${checked}">${todo.name}</span>
-      </label>
-      <button class="delete-btn" data-index="${index}" onclick="remove(this)"><i class="fa fa-times"></i></button>
-    </li>
-  `;
-}
-
-function showTodos() {
-  if (todosJson.length == 0) {
-    todosHtml.innerHTML = '';
-    emptyImage.style.display = 'block';
-  } else {
-    todosHtml.innerHTML = todosJson.map(getTodoHtml).join('');
-    emptyImage.style.display = 'none';
-  }
-}
-
-function addTodo(todo)  {
-  input.value = "";
-  todosJson.unshift({ name: todo, status: "pending" });
-  localStorage.setItem("todos", JSON.stringify(todosJson));
-  showTodos();
-}
-
-input.addEventListener("keyup", e => {
-  let todo = input.value.trim();
-  if (!todo || e.key != "Enter") {
-    return;
-  }
-  addTodo(todo);
-});
-
-addButton.addEventListener("click", () => {
-  let todo = input.value.trim();
-  if (!todo) {
-    return;
-  }
-  addTodo(todo);
-});
-
-function updateStatus(todo) {
-  let todoName = todo.parentElement.lastElementChild;
-  if (todo.checked) {
-    todoName.classList.add("checked");
-    todosJson[todo.id].status = "completed";
-  } else {
-    todoName.classList.remove("checked");
-    todosJson[todo.id].status = "pending";
-  }
-  localStorage.setItem("todos", JSON.stringify(todosJson));
-}
-
-function remove(todo) {
-  const index = todo.dataset.index;
-  todosJson.splice(index, 1);
-  showTodos();
-  localStorage.setItem("todos", JSON.stringify(todosJson));
-}
-
-filters.forEach(function (el) {
-  el.addEventListener("click", (e) => {
-    if (el.classList.contains('active')) {
-      el.classList.remove('active');
-      filter = '';
-    } else {
-      filters.forEach(tag => tag.classList.remove('active'));
-      el.classList.add('active');
-      filter = e.target.dataset.filter;
+function addTask(){
+    if(inputBox.value == ''){
+        alert('Please Enter the Text')
     }
-    showTodos();
-  });
-});
+    else{
+        const task = document.createElement('li')
+        task.textContent = inputBox.value;
+        listContainer.appendChild(task)
+        // inputBox.value = ''
+        let span = document.createElement('span')
+        span.textContent = "\u00d7"
+        task.appendChild(span)
+        span.style.right = '0px';
+    }
+    inputBox.value = '';
+    saveData()
+}
 
-deleteAllButton.addEventListener("click", () => {
-  todosJson = [];
-  localStorage.setItem("todos", JSON.stringify(todosJson));
-  showTodos();
-});
+listContainer.addEventListener('click', (e)=>{
+    if(e.target.tagName === 'LI'){
+        e.target.classList.toggle("checked")
+        saveData()
+    }
+    else if(e.target.tagName === 'SPAN'){
+        e.target.parentElement.remove()
+        saveData()
+    }
+})
+
+function saveData(){
+    localStorage.setItem("data",listContainer.innerHTML)
+}
+
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+
+showTask()
